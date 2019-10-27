88c6c163c7810fd8a1a9f3cd5b09a4b4e9022176
Erick Costa <erick.costa.ti@gmail.com>
2019-10-27 18:24:21 -0300

    Update jwt autentication

---
+++ modulo02/src/app/controllers/UserController.js
@@ -0,0 +1,48 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(300).json({ error: 'User email already exists' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (!user) {
+      return res.status(401).json({ error: 'User nor found' });
+    }
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(401).json({ error: 'User email already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Erick',
-    email: 'erick.costa.ti@gmail.com',
-    password_hash: '1234567890',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json({ user });
-});
+routes.use(authMiddleware);
+
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
