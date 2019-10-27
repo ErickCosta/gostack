290026fc85f77f4a513d13a2fce355c127c495bd
Erick Costa <erick.costa.ti@gmail.com>
2019-10-27 09:35:38 -0300

    Update project strucutre

---
+++ modulo02/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "linebreak-style": 0,
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next"}]
+  },
+};
