a46eb7e1dc98727dd9d6b92d382b751ebbf6ea82
Erick Costa <erick.costa.ti@gmail.com>
2019-11-09 22:42:24 -0300

    Notifications register

--- modulo02/src/app/controllers/AppointmentsController.js
+++ modulo02/src/app/controllers/AppointmentsController.js
@@ -1,8 +1,10 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointments';
 import User from '../models/User';
 import File from '../models/File';
+import NotificationSchema from '../schemas/Notification';
 
 class AppointmentsController {
   async index(req, res) {
@@ -73,6 +75,18 @@ class AppointmentsController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    const { name } = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await NotificationSchema.create({
+      content: `Novo agendamento para ${name} para o ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
